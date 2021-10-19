412ab4908d84d1cc42f3247af7ac9d072cf462c3
shaibaz <shaibaz@LAPTOP-UINCFN28>
2021-10-19 23:54:00 +0530

    First comit for credo

---
+++ Credo/src/main/java/com/crm/credo/genericUtils/CreateOrgTest.java
@@ -0,0 +1,11 @@
+package com.crm.credo.genericUtils;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	@Test
+	public void createOrg() {
+		System.out.println("hello create org");
+	}
+
+}
