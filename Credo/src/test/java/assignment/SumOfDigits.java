37f623f904020275d6d26df24155f6b88feda5c3
Iamshaibaz <md.shaibaz.200@gmail.com>
2023-01-21 21:58:24 +0530

    Sum of Digits inside Digit

--- Credo/src/test/java/assignment/SumOfDigits.java
+++ Credo/src/test/java/assignment/SumOfDigits.java
@@ -4,13 +4,15 @@ public class SumOfDigits {
 
 	public static void main(String[] args)
 	{
-		int n=1232;
-		int sum=0;
+		int n=12323;
+		int sum = 0;
 		while(n>0) {
+			
 			int dig = n%10;
 			sum = sum+dig;
 			n=n/10;
-		}
-		System.out.println(sum);
+		}System.out.println(sum);
+		
+		
 	}
 }
